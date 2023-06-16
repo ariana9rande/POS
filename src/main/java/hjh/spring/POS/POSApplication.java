bd5810a09e464bd7f50c0b4290a40b1ec04b6624
hjh970422 <hjh970422@gmail.com>
2023-06-16 23:18:45 +0900

    230616

--- src/main/java/hjh/spring/POS/POSApplication.java
+++ src/main/java/hjh/spring/POS/POSApplication.java
@@ -1,16 +1,13 @@
 package hjh.spring.POS;
 
-import hjh.spring.POS.configuration.JavaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class POSApplication
 {
-
-	public static void main(String[] args) {
-		SpringApplication.run(POSApplication.class, args);
-	}
-
+    public static void main(String[] args)
+    {
+        SpringApplication.run(POSApplication.class, args);
+    }
 }
